303ea21e3b5f2108788a167aae266812f09b406e
SenerDemiral <sener.demiral@gmail.com>
2020-10-22 13:20:49 +0300

    Third

---
+++ DataLibrary/Models/TTmodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataLibrary.Models
+{
+    [Table("TT")]
+    public class TTmodel
+    {
+        [Key]
+        public int TtID { get; set; }
+        public int DtID { get; set; }
+        public string Ad { get; set; }
+        public string Sablon { get; set; }
+    }
+}
